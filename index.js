b551c2cb2c812ff718cba82b80fa8822bfe3c9ca
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-09 00:36:18 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// Challenge 1: Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Challenge 2: Random
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Challenge 3: Modulo
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Challenge 4: Max
+const max = Math.max(1, 2, 3, 4, 20);
+
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max,
+};
